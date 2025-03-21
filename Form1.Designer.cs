cdb913f92686562d6d40d0e37c0970d45ee0fc86
evgenii-cher <evgg90@gmail.com>
2025-03-21 16:28:41 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(27, 12);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(640, 480);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            this.imageBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.imageBox1_MouseClick);
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.Location = new System.Drawing.Point(746, 7);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(640, 480);
+            this.imageBox2.TabIndex = 2;
+            this.imageBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(680, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(55, 35);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(675, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(60, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(673, 84);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(67, 26);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Scale";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(673, 116);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(67, 33);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Shift";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(674, 155);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(66, 35);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Projection";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1507, 695);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.ImgHash;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+
+
+namespace lab2
+{
+    public partial class Form1 : Form
+    {
+        Image<Bgr, byte> srcImg;
+        double move = 0;
+        PointF[] pts = new PointF[4];
+        int c = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            var result = openFileDialog.ShowDialog(); // открытие диалога выбора файла
+            if (result == DialogResult.OK) // открытие выбранного файла
+            {
+                string fileName = openFileDialog.FileName;
+                srcImg = new Image<Bgr, byte>(fileName).Resize(640, 480, Inter.Linear);
+
+                imageBox1.Image = srcImg;
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double k = Double.Parse(textBox1.Text);
+            Image<Bgr, byte> scaledImg = new Image<Bgr,byte>((int)(srcImg.Width * k), (int)(srcImg.Height * k));
+
+            for(int i = 0; i < scaledImg.Width; i++)
+                for(int j = 0; j < scaledImg.Height; j++)
+                {
+                    double I = (i / k);
+                    double J = (j / k);
+                    double baseI = Math.Floor(I);
+                    double baseJ = Math.Floor(J);
+
+                    if (baseI >= srcImg.Width - 1) continue;
+                    if (baseJ >= srcImg.Height -1) continue;
+
+                    double rI = I - baseI;
+                    double rJ = J - baseJ;
+                    double irI = 1 - rI;
+                    double irJ = 1 - rJ;
+
+                    Bgr c1 = new Bgr();
+                    c1.Blue = srcImg.Data[(int)baseJ, (int)baseI, 0] * irI + srcImg.Data[(int)baseJ, (int)(baseI + 1), 0] * rI;
+                    c1.Green = srcImg.Data[(int)baseJ, (int)baseI, 1] * irI + srcImg.Data[(int)baseJ, (int)(baseI + 1), 1] * rI;
+                    c1.Red = srcImg.Data[(int)baseJ, (int)baseI, 2] * irI + srcImg.Data[(int)baseJ, (int)(baseI + 1), 2] * rI;
+
+                    Bgr c2 = new Bgr();
+                    c2.Blue = srcImg.Data[(int)(baseJ +1), (int)baseI,0] * irI + srcImg.Data[(int)(baseJ+1), (int)(baseI + 1), 0] * rI;
+                    c2.Green = srcImg.Data[(int)(baseJ + 1), (int)baseI, 1] * irI + srcImg.Data[(int)(baseJ + 1), (int)(baseI + 1), 1] * rI;
+                    c2.Red = srcImg.Data[(int)(baseJ + 1), (int)baseI, 2] * irI + srcImg.Data[(int)(baseJ + 1), (int)(baseI + 1), 2] * rI;
+
+                    Bgr c = new Bgr();
+                    c.Blue = c1.Blue * irJ + c2.Blue * rJ;
+                    c.Green = c1.Green * irJ + c2.Green * rJ;
+                    c.Red = c1.Red * irJ + c2.Red * rJ;
+
+                    scaledImg[j, i] = c;
+                }
+            imageBox2.Image = scaledImg;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // коэффициенты масштабирования
+            float sX = 1.5f;
+            float sY = 1.5f;
+            // создание нового изображения
+            // высота и ширина нового изображения увеличивается в sX и sY раз соответственно
+            var newImage = new Image<Bgr, byte>((int)(srcImg.Width * sX),
+            (int)(srcImg.Height * sY));
+            for (int x = 0; x < srcImg.Width; x++)
+            {
+                for (int y = 0; y < srcImg.Height; y++)
+                {
+                    // вычисление новых координат пикселя
+                    int newX = (int)(x * sX);
+                    int newY = (int)(y * sY);
+                    // копирование пикселя в новое изображение
+                    newImage[newY, newX] = srcImg[y, x];
+                }
+                imageBox2.Image = newImage;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            move = move + 0.1;
+            // создание нового изображения
+            // высота и ширина нового изображения увеличивается в sX и sY раз соответственно
+            // var newImage = srcImg.CopyBlank();
+             var newImage = new Image<Bgr, byte>(srcImg.Width, srcImg.Height);
+           // var newImage = srcImg.Copy();
+
+            for (int x = 0; x < srcImg.Width; x++)
+            {
+                for (int y = 0; y < srcImg.Height; y++)
+                {
+                    // вычисление новых координат пикселя
+                    int newX = (int)(x + move * (srcImg.Height - y));
+                    if (newX >= srcImg.Width) newX = srcImg.Width-1;
+               // textBox2.Text = newX.ToString();
+                    int newY = y;
+                   // if (newY >= 480) newY = 480;
+                  
+                    // копирование пикселя в новое изображение
+                    newImage[newY, newX] = srcImg[y, x];
+                }
+                imageBox2.Image = newImage;
+            }
+        }
+
+        private void imageBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            var imgCopy = srcImg.Copy();
+
+            int x = (int)(e.Location.X / imageBox1.ZoomScale);
+            int y = (int)(e.Location.Y / imageBox1.ZoomScale);
+
+            pts[c] = new Point(x, y);
+            c++;
+            if (c >= 4)
+                c = 0;
+           // Point center = new Point(x, y);
+            int radius = 2;
+            int thickness = 2;
+            var color = new Bgr(Color.Blue).MCvScalar;
+
+            // функция, рисующая на изображении круг с заданными параметрами
+            for(int i = 0; i < 4; i++)
+            CvInvoke.Circle(imgCopy, new Point((int)pts[i].X, (int)pts[i].Y), radius, color, thickness);
+
+            imageBox1.Image = imgCopy;
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+         //   var srcPoints = new PointF[]
+{
+             // четыре точки на исходном изображении
+};
+            var destPoints = new PointF[]
+            {
+             // плоскость, на которую осуществляется проекция,
+             // задаётся четыремя точками
+             new PointF(0, 0),
+             new PointF(0, srcImg.Height - 1),
+             new PointF(srcImg.Width - 1, srcImg.Height - 1),
+             new PointF(srcImg.Width - 1, 0)
+            };
+            // функция нахождения матрицы гомографической проекции
+            var homographyMatrix = CvInvoke.GetPerspectiveTransform(pts, destPoints);
+            var destImage = new Image<Bgr, byte>(srcImg.Size);
+            CvInvoke.WarpPerspective(srcImg, destImage, homographyMatrix, destImage.Size);
+
+            imageBox2.Image = destImage;
+        }
+    }
+}
